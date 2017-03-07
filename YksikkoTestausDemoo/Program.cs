c03d76a7199b54c5b67cd0366efed46f3a3a70bc
K2460 <K2460@ICT-421-WS-37>
2017-03-07 15:59:29 +0200

    ooo

---
+++ YksikkoTestausDemoo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YksikkoTestausDemoo
+{
+    interface ICalculator
+    {
+        int Add(int numero1, int numero2);
+        int Multiply(int numero1, int numero2);
+    }
+    //luokka joka toteuttaa ICalculator-rajapinnan
+    public class Calculator : ICalculator
+    {
+        public int Add(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+        public int Multiply(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+    class TestiPeti
+    {
+
+    }
+  
+        static void Main(string[] args)
+        {
+            //kokeillaan luokkaa Calculator
+            Calculator laskin = new YksikkoTestausDemoo.Calculator();
+            int tulos = laskin.Add(10, 20);
+        }
+    }
+}
